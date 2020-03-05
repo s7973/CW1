ae5217faab857626a654ab29aba453b7f667eb4b
s7973 <s7973@pjwstk.edu.pl>
2020-03-05 13:19:59 +0100

    Pierwszy commit

---
+++ CW1/CW1/Person.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cw1
+{
+    class Person
+    {
+        public string name { get; set; }
+    }
+}
---
+++ CW1/CW1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Cw1
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            //var osoba = new Person { name = "Rafał" };
+
+            var httpClient = new HttpClient();
+            var url = args.Length > 0 ? args[0] : "https://www.pja.edu.pl";
+            var response = await httpClient.GetAsync(url);
+
+            if (response.IsSuccessStatusCode)
+            {
+                string content = await response.Content.ReadAsStringAsync();
+                var regex = new Regex("[a-z]+[a-z0-9]*@[a-z0-9]+\\.[a-z]+", RegexOptions.IgnoreCase);
+
+                var matches = regex.Matches(content);
+
+                foreach (var match in matches)
+                {
+                    Console.WriteLine(match.ToString());
+                }
+
+            }
+        }
+    }
+}
